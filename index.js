35bc15021351775cca2faec73e5cf03a5c69ed70
oussama1920-boussetta <76227614+oussama1920-boussetta@users.noreply.github.com>
2021-02-17 10:54:57 +0100

    commit2

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ const app = express()
 app.use(express.static('public'));
 app.listen(8080,()=>console.log('ssssss'))
 
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var d = new Date();
+var dayName = days[d.getDay()];
+
+var hourDate = d.getHours();
+
+if (['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'].includes(dayName) && (hourDate >= 9 && hourDate <= 17)) {
+    next();
+} else {
+    res.render('/horsService', {
+        namePage: "horsService"
+    });
+
+}
+
